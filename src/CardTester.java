f1aea37b84d00f48afa4773681cce1ccfc393c79
Matthew Cadol <cadolmb@s.dcsdk12.org>
2020-03-26 16:37:15 -0600

    activity 1

--- src/CardTester.java
+++ src/CardTester.java
@@ -8,6 +8,12 @@ public class CardTester {
 	 *	@param args is not used.
 	 */
 	public static void main(String[] args) {
-		/* *** TO BE IMPLEMENTED IN ACTIVITY 1 *** */
+		Card aceOfSpades = new Card("Ace", "Spades", 1);
+		Card queenOfHearts = new Card("Queen", "Hearts", 12);
+		Card sevenOfDiamonds = new Card("Seven", "Diamonds", 7);
+
+		System.out.println(aceOfSpades.toString());
+		System.out.println(sevenOfDiamonds.suit());
+		System.out.println(queenOfHearts.rank());
 	}
 }
